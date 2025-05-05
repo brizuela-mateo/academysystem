709555ee8e01fbe487278a4d228d2f0b2d3c1671
brizuela-mateo <mateorexto2001@gmail.com>
2025-05-05 14:52:22 -0300

    Initial commit

---
+++ src/main/java/com/profebrian/academysystem/AcademysistemApplication.java
@@ -0,0 +1,13 @@
+package com.profebrian.academysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademysistemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademysistemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/profebrian/academysystem/AcademysistemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profebrian.academysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademysistemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
