5f0b35c95181cbb1d4cc399b57c4eaf1ad2fc7c8
brizuela-mateo <mateorexto2001@gmail.com>
2025-09-01 15:36:47 -0300

    Add GlobalExceptionHandler

---
+++ src/main/java/com/profebrian/academysystem/exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.profebrian.academysystem.exception;
+
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({ResourceNotFoundException.class})
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+}
