7c3737efa0832bded1e2ff5217bf439787658a59
brizuela-mateo <mateorexto2001@gmail.com>
2025-09-16 20:11:10 -0300

    Add Student DTO

---
+++ src/main/java/com/profebrian/academysystem/student/dto/StudentSaveDTO.java
@@ -0,0 +1,23 @@
+package com.profebrian.academysystem.student.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.util.Date;
+
+public record StudentSaveDTO(@NotNull @Size(max = 50)
+                                 String firstName,
+                             @NotNull @Size(max = 50)
+                                 String lastName,
+                             @NotNull
+                                 Date dateOfBirth,
+                             @Size(max = 15)
+                                 String ruc,
+                             @Size(max = 100)
+                                 String ruc_owner,
+                             @NotNull
+                                 Integer documentTypeId,
+                             @NotNull
+                                 Integer city,
+                             Integer tutorId) {
+}
