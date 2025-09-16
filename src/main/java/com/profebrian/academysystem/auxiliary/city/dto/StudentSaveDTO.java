6367a1a7d25112891b7299a33488edb323f6792a
brizuela-mateo <mateorexto2001@gmail.com>
2025-09-16 20:13:02 -0300

    Add Thymeleaf dependency

--- src/main/java/com/profebrian/academysystem/auxiliary/city/dto/StudentSaveDTO.java
+++ src/main/java/com/profebrian/academysystem/auxiliary/city/dto/StudentSaveDTO.java
@@ -1,4 +1,4 @@
-package com.profebrian.academysystem.auxiliary.city.dto;
+package com.profebrian.academysystem.auxiliary.city;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
